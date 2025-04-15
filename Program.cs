8c0d99f72229824911b29f6f4cebed789304bacb
Sarahdraz <130856810+Sarahdraz@users.noreply.github.com>
2025-04-15 20:10:34 +0200

    Add files via upload
    A simple C# task that reads a set of numbers from the user and calculates the minimum and maximum values.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Task1;
+int Num;
+int Max;
+int Min;
+Task3.Main();
+Task2.Main();
+Console.WriteLine("Please Enter Your Numbers:");
+//Num = 0;
+Console.WriteLine("Enter Number 1:"); 
+Num = int.Parse(Console.ReadLine()); 
+// It caused an error when starting the for loop at 1 because Num was not defined. So,I initialized Num by assigning it to Max and Min as an initial value
+Max = Min = Num;
+
+for (int i = 2; i <= 10; i++)
+{
+    Console.WriteLine($"Enter Number {i} : ");
+    Num=int.Parse(Console.ReadLine());
+    if (Num< Min)
+    {
+     Min = Num;
+    }
+    if (Num > Max)
+    {
+        Max = Num;
+    }
+  //  Console.WriteLine($"Minimum: {Min}, Maximum: {Max}");
+}
+Console.WriteLine($"Minimum: {Min}, Maximum: {Max}");
